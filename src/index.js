0f79004b543271bbff55be9cc87b85329cfb3415
YouJeen-ru <1.rouch@mail.ru>
2020-12-25 17:38:13 +0300

    add new method toHTML, getRoot

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import './scss/index.scss'
 import {Excel} from "@/components/excel/Excel";
+import {Header} from "@/components/header/Header";
+import {Toolbar} from "@/components/toolbar/Toolbar";
+import {Formula} from "@/components/formula/Formula";
+import {Table} from "@/components/table/Table";
 
 const excel = new Excel('#app', {
-    components: []
+    components: [Header, Toolbar, Formula, Table]
 })
-console.log('Excel', excel)
+excel.render()
