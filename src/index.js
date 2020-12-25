311f37277527a9471ab557eae1823ac767eee950
YouJeen-ru <1.rouch@mail.ru>
2020-12-25 17:11:48 +0300

    add base organization app

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 import './scss/index.scss'
+import {Excel} from "@/components/excel/Excel";
 
-console.log('Work')
+const excel = new Excel('#app', {
+    components: []
+})
+console.log('Excel', excel)
