33e722a47932d47932d084ee71ae2c1e55c86a60
YouJeen-ru <1.rouch@mail.ru>
2020-12-27 10:25:30 +0300

    testing events mousedown, mousemove, mouseup

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -3,7 +3,32 @@ import {createTable} from "@/components/table/table.template";
 
 export class Table extends ExcelComponent {
     static className = 'excel__table'
+
+    constructor($root) {
+        super($root, {
+            listeners: ['click', 'mousedown', 'mousemove', 'mouseup']
+        });
+    }
+
     toHTML() {
         return createTable(20)
+    }
+
+    onClick() {
+        console.log('click')
+
+    }
+
+    onMousedown(event) {
+        console.log('mousedown', event.target)
+
+    }
+
+    onMousemove() {
+        console.log('mousemove')
+    }
+
+    onMouseup() {
+        console.log('mouseup')
     }
 }
