077cf004b0106e23564001c6d6cf625cac28bd24
YouJeen-ru <1.rouch@mail.ru>
2020-12-27 12:19:50 +0300

    resize row

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -27,6 +27,8 @@ export class Table extends ExcelComponent {
                 const delta = e.pageX - coords.right
                 const value = coords.width + delta
                 $parent.$el.style.width = value + 'px'
+                document.querySelectorAll(`[data-col="${$parent.data.col}"]`)
+                    .forEach(el => el.style.width = value + 'px')
             }
 
             document.onmouseup = () => {
