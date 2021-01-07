bac24b63cf5f08830e27e3c6f0be6288a71240f1
YouJeen-ru <1.rouch@mail.ru>
2021-01-07 16:25:43 +0300

    Formula + Table context text start realized

--- src/components/formula/Formula.js
+++ src/components/formula/Formula.js
@@ -3,10 +3,11 @@ import {ExcelComponent} from "@core/ExcelComponent";
 export class Formula extends ExcelComponent {
     static className = 'excel__formula'
 
-    constructor($root) {
+    constructor($root, options) {
         super($root, {
             name: 'Formula',
-            listeners: ['input', 'click']
+            listeners: ['input'],
+            ...options
         });
     }
 
@@ -20,10 +21,8 @@ export class Formula extends ExcelComponent {
     }
 
     onInput(event) {
-        console.log('Formula: onInput ', event)
+        const text = event.target.textContent.trim()
+        this.emitter.emit('it is working', text)
     }
 
-    onClick() {
-        console.log('click')
-    }
 }
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -2,6 +2,12 @@ import {ExcelComponent} from "@core/ExcelComponent";
 
 export class Header extends ExcelComponent {
     static className = 'excel__header'
+    constructor($root, options) {
+        super($root, {
+            name: 'Header',
+            ...options
+        });
+    }
     toHTML() {
         return `
         
