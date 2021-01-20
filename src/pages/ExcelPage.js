19b9f96ad72b9e4d81e9dfe677ebfa70cd4cd491
YouJeen-ru <1.rouch@mail.ru>
2021-01-20 12:53:46 +0300

    add new dependencies

--- src/pages/ExcelPage.js
+++ src/pages/ExcelPage.js
@@ -22,7 +22,6 @@ export class ExcelPage extends Page {
         const store = createStore(rootReducer, initialState)
 
         const stateListener = debounce(state => {
-            console.log('App state:', state)
             storage(storageName(params), state)
         }, 300)
 
