5c2386e3e7f71f4c904b1fa1af31e793a830087f
YouJeen-ru <1.rouch@mail.ru>
2021-01-20 12:02:45 +0300

    remove and exit button realization

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -3,13 +3,14 @@ import {$} from "@core/dom";
 import {changeTitle} from "@/redux/actions";
 import {defaultTitle} from "@/constans";
 import {debounce} from "@core/utils";
+import {ActiveRoute} from "@core/routes/ActiveRoute";
 
 export class Header extends ExcelComponent {
     static className = 'excel__header'
     constructor($root, options) {
         super($root, {
             name: 'Header',
-            listeners: ['input'],
+            listeners: ['input', 'click'],
             ...options
         });
     }
@@ -21,21 +22,35 @@ export class Header extends ExcelComponent {
     toHTML() {
         const title = this.store.getState().title || defaultTitle
         return `
-        
-       <input type="text" class="input" value="${title}" />
+      <input type="text" class="input" value="${title}" />
 
-            <div>
+      <div>
 
-                <div class="button">
-                    <i class="material-icons">delete</i>
-                </div>
+        <div class="button" data-button="remove">
+          <i class="material-icons" data-button="remove">delete</i>
+        </div>
 
-                <div class="button">
-                    <i class="material-icons">exit_to_app</i>
-                </div>
+        <div class="button" data-button="exit">
+          <i class="material-icons" data-button="exit">exit_to_app</i>
+        </div>
 
-            </div>
-        `
+      </div>
+    `
+    }
+
+    onClick(event) {
+        const $target = $(event.target)
+
+        if ($target.data.button === 'remove') {
+            const decision = confirm('Вы действительно хотите удалить эту таблицу?')
+
+            if (decision) {
+                localStorage.removeItem('excel:' + ActiveRoute.param)
+                ActiveRoute.navigate('')
+            }
+        } else if ($target.data.button === 'exit') {
+            ActiveRoute.navigate('')
+        }
     }
 
     onInput(event) {
--- src/pages/dashboard.functions.js
+++ src/pages/dashboard.functions.js
@@ -1,7 +1,11 @@
-function toHTML() {
+import {storage} from "@core/utils";
+
+function toHTML(key) {
+    const model = storage(key)
+    const id = key.split(':')[1]
     return `
      <li class="db__record">
-                    <a href="#">Таблица номер 1</a>
+                    <a href="#excel/${id}">${model.title}</a>
                     <strong>24.12.2020</strong>
                 </li>
     `
