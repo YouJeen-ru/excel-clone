ad2fe80abb7db06cd4bd4083db54d28a523eaf3c
YouJeen-ru <1.rouch@mail.ru>
2021-01-25 13:37:19 +0300

    unit-testing add new dependencies

---
+++ src/core/store/createStore.spec.js
@@ -0,0 +1,9 @@
+import {createStore} from "./createStore";
+
+
+describe('TEST', () => {
+    test('test', () => {
+        const store = createStore(() => {}, {})
+        expect(store).toBeDefined()
+    })
+})
